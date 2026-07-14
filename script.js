8f3a6347ded71d8f8e9609658ef66b09dbbf7b44
gebekmagomedov0-hash <gebekmagomedov0@gmail.com>
2026-07-14 15:47:42 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function answer(text) {
-    let chat = document.getElementById("chat");
-
-    let message = document.createElement("div");
-    message.className = "answer";
-    message.innerHTML = "<b>Ты:</b><br>" + text;
-
-    chat.appendChild(message);
-}
